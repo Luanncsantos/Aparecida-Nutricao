0a8db0db35984a9b84b28fe396508fb84b944df2
Luan Nascimento <61527988+Luanns@users.noreply.github.com>
2022-10-05 21:39:49 -0300

    Envio dos arquivos

---
+++ Codigos.js
@@ -0,0 +1,20 @@
+<script></script> /
+<script src="js/arquivo.js"></script> /
+// sempre que for usar o JavaScript deixar entre essas tags
+*Javascript deve ser colocado no final do body*
+*Aconselhável deixar Javascript separado do arquivo do HTML, assim como o css*
+alert("Olá Mundo"); // Para abrir um popup
+console.log("Olá Mundo"); // Deixar uma mensagem no console do navegador//
+document // Como o navegador enxerga o HTML utilizado por ele para renderizar a página
+querySelector("(O que eu quero filtrar)"); // Para filtrar alguma coisa (Apenas 1)
+var // Para criar variáveis
+variavel .textContent // Para alterar o valor de alguma variável
+// Para chamar uma classe -> .classe (Pode ser usada no querySelector)
+// Mais aconselhável chamar uma classe do que uma tag
+querySelectorAll("(O que eu quero filtrar)"); // Para filtrar todos daquele tipo
+.toFixed // Serve para restringir quantas casas decimais irão ser exibidas.
+event.preventDefault(); //Serve para impedir que um evento haja da forma que 
+//ele havia sido programado.
+addEventListener() // Ele ouve a ação de um determinado evento.
+appendChild // Adicionar outras filhas a uma tag pai
+createElement // Usado no Javascript para criar um elemento no html.
